b153311cdeccc28dfe0b300e972e4334416ac846
Lucas Hahne <lucashahne@MacBook-Air-de-Lucas.local>
2024-11-25 15:46:09 -0300

    Adiciona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para treinar e melhorar minhas habilidades de programação!
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1>Troquei a frase!!!!</h1>
-
+  return <h1>Troquei a frase!!!!</h1>;
 }
 
 export default Home;
